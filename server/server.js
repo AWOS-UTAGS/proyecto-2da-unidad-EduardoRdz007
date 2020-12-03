cc7353adc7b71e5c2aaee1b9bd7f37f39798d1f4
EduardoRdz007 <edurdz010@gmail.com>
2020-12-03 10:33:29 -0600

    Login con password encriptado

--- server/server.js
+++ server/server.js
@@ -14,8 +14,10 @@ app.get('/', function (req, res) {
   res.send('<h1> Bienvenido a mi servidor Rest </h1>');
 });
 
-app.use(require('./routes/usuario'))
-app.use(require('./routes/categoria'))
+app.use(require('./routes/usuario'));
+app.use(require('./routes/categoria'));
+app.use(require('./routes/login'));
+
 
  mongoose.connect('mongodb://localhost:27017/cafeteria', {
     useNewUrlParser: true,
