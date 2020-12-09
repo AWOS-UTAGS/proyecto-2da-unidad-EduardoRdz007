89d0a732186ecfa16523f1c3d13e28812e22f964
EduardoRdz007 <edurdz010@gmail.com>
2020-12-09 15:35:35 -0600

    Se agrego el cors

--- server/server.js
+++ server/server.js
@@ -2,8 +2,22 @@ require('./config');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const app = express();
+const app = express(),
+const cors = require('cors');
 
+// Habilita CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+  next();
+});
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
